21fe5ddf0e94f4960a87118456bffd710779240e
Naznin Akter <akternaznin32@gmail.com>
2026-09-07 01:25:26 +0600

    link changed

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-SQLALCHEMY_DATABASE_URL="postgresql://postgres:project@localhost/expense_tracker_database"
+SQLALCHEMY_DATABASE_URL="postgresql://postgres.uonbrirxkrwdboqhtddl:[fstapiprojec]@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres"
 engine=create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal=sessionmaker(autoflush=False, autocommit=False, bind=engine)
 Base=declarative_base()
